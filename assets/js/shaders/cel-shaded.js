02a62560782f420a5b809ee1afee3a6e2dfd2c53
Void Drifter (VoltacceptYT) <voltaccept@gmail.com>
2025-02-22 13:17:55 -0500

    Transfer to VoltarianTCG/Update

--- assets/js/shaders/cel-shaded.js
+++ assets/js/shaders/cel-shaded.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const character = document.querySelector('.character');
     
     character.addEventListener('mouseover', () => {
-        character.style.filter = 'brightness(1.5) contrast(2)';
+        character.style.filter = ' contrast(2) brightness(1.5) saturate(2) sepia(0.7) saturate(3) hue-rotate(-20deg)';
     });
     
     character.addEventListener('mouseout', () => {
-        character.style.filter = 'brightness(1.3) contrast(2)';
+        character.style.filter = ' contrast(2) brightness(1.3) saturate(2) sepia(0.7) saturate(3) hue-rotate(-20deg)';
     });
 });
